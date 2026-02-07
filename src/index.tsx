e0b9a1ab5cefea588990e66f371d36ed83747709
ablair264 <blair@hotmail.co.uk>
2026-02-07 02:01:09 +0000

    update

--- src/index.tsx
+++ src/index.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom';
+import { NuqsAdapter } from 'nuqs/adapters/react';
 import App from './App';
 import './index.css';
 
@@ -10,8 +11,10 @@ const root = ReactDOM.createRoot(
 
 root.render(
   <React.StrictMode>
-    <BrowserRouter>
-      <App />
-    </BrowserRouter>
+    <NuqsAdapter>
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>
+    </NuqsAdapter>
   </React.StrictMode>
 );
